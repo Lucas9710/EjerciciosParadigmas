cd63b7d69ebfc000fc31ec798b9713ddc45bdf18
lucas <lucasnunez9710@gmail.com>
2021-05-23 01:08:02 -0300

    ejercicio16

---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//16. Se leen dos números y se necesita informarlos en orden ascendentes;
+
+namespace Ejercicio16
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("ingrese el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el primer numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            if(num1 > num2)
+            {
+                Console.WriteLine(num2 + " " + num1);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " " + num2);
+            }
+
+
+
+        }
+    }
+}
