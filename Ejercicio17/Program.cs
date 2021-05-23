36e61b57fcf5b4aaf2d862033684b11d9d259876
lucas <lucasnunez9710@gmail.com>
2021-05-23 01:35:58 -0300

    ejercicio 17

---
+++ Ejercicio17/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+//Ingresar 3 números enteros e informar el menor de ellos. CONFECCIONAR PRUEBA DE ESCRITORIO
+
+namespace Ejercicio17
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+
+            Console.WriteLine("ingrese el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el segundo numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("ingrese el tercer numero");
+            num3 = int.Parse(Console.ReadLine());
+
+            if(num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("el numero " + num1 + " es mayor");
+
+            }
+
+
+            if(num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("el numero " + num2 + " es mayor");
+            }
+
+
+            if (num3 > num1 && num3 > num2)
+            {
+                Console.WriteLine("el numero " + num3 + " es mayor");
+            }
+        }
+    }
+}
