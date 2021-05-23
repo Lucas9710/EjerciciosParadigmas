90474d610323aba53d8fb6cb523def7c5b97a005
lucas <lucasnunez9710@gmail.com>
2021-05-23 01:39:28 -0300

    ejercicio 18

---
+++ Ejercicio18/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio18
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
